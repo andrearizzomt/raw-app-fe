4fb0716191927612ca1272099bdb61858dde6a8d
Andrea Rizzo <andrea.rizzo@casinogrounds.com>
2025-04-18 19:51:36 +0200

    create Landing page with Coming Soon text and updated relative assets like background logos favicons fonts etc

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Static HTML export creating /out folder used to deploy the landing page on Vercel
+  // output: "export",
 };
 
 export default nextConfig;
